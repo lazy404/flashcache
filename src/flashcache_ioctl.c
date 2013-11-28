1c7c76ddf8e3b13aca3e558eb5a20e26893591f7
Michal Grzedzicki <lazy404@gmail.com>
2013-11-28 01:09:24 +0100

    fix null dereference in openvz VE

--- src/flashcache_ioctl.c
+++ src/flashcache_ioctl.c
@@ -338,10 +338,11 @@ flashcache_uncacheable(struct cache_c *dmc, struct bio *bio)
 				goto out;
 			}
 		}
-		if (IOPRIO_PRIO_CLASS(current->io_context->ioprio) == IOPRIO_CLASS_NOCACHE) {
-		    dontcache = 1;
-		    goto out;
-		}
+		if (current->io_context)
+		    if (IOPRIO_PRIO_CLASS(current->io_context->ioprio) == IOPRIO_CLASS_NOCACHE) {
+			dontcache = 1;
+			goto out;
+		    }
 
 		/* Finally, if we are neither in a whitelist or a blacklist,
 		 * do a final check to see if this is sequential i/o.  If
